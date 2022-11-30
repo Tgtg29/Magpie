ea1d175f2fb81553f4c67b4bd8701b1971591939
Tgtg29 <tgtg7871@gmail.com>
2022-11-29 23:07:09 -0800

    Almost There

--- Activity2/Magpie2.java
+++ Activity2/Magpie2.java
@@ -42,6 +42,18 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("dog") >= 0 || statement.indexOf("cat") >=0){
+			response = "Tell me more about your pets";
+		}
+		else if (statement.indexOf("Mr.") >= 0){
+			response = "He sounds like a good teacher.";
+		}
+		else if (statement.indexOf("Mrs.") >= 0){
+			response = "She sounds like a good teacher.";
+		}
+		else if (statement.trim() == ("") ){
+			response = "Say something, please.";
+		}
 		else
 		{
 			response = getRandomResponse();
@@ -55,7 +67,7 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -75,6 +87,14 @@ public class Magpie2
 		else if (whichResponse == 3)
 		{
 			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "That's Awesome!!";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "No Way!!";
 		}
 
 		return response;
